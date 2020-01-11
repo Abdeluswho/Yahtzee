b74ff989fe7667581c722a05818ac010925071b3
A.Sofiane Guerfi <schguerfi@gmail.com>
2020-01-10 20:46:50 -0800

    GameLog

--- src/Game.js
+++ src/Game.js
@@ -42,13 +42,15 @@ class Game extends Component {
 
     toggleLocked = (idx) => {
         // toggle whether idx is in locked or not
-        this.setState(st => ({
-            locked: [
-            ...st.locked.slice(0, idx),
-            !st.locked[idx],
-            ...st.locked.slice(idx + 1)
-            ]
-        }));
+        if (this.state.rollsLeft > 0 ) {
+            this.setState(st => ({
+                locked: [
+                ...st.locked.slice(0, idx),
+                !st.locked[idx],
+                ...st.locked.slice(idx + 1)
+                ]
+            }));
+        }
     }
 
     doScore = (rulename, ruleFn) => {
@@ -76,7 +78,7 @@ class Game extends Component {
                         <div className='Game-button-wrapper'>
                             <button 
                                 className='Game-reroll'
-                                disabled={this.state.locked.every(x => x)}
+                                disabled={this.state.locked.every(x => x) }
                                 onClick={this.roll}
                             >
                                {this.state.rollsLeft} Rerolls left!
